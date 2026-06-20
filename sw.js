db9f0188fd29f982558df9e740f1e85b3152bd72
villarruel449-eng <villarruel449@gmail.com>
2026-06-20 10:46:25 -0300

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,25 +1,54 @@
-const CACHE_NAME = 'aureo-cache-v2';
+const CACHE_NAME = 'aureo-cache-v3';
 const ASSETS = [
-  'index.html',
-  'admin.html',
-  'historial.html',
-  'franquicias.html',
-  'manifest.json',
-  'icono-192.png',
-  'icono-512.png'
+  './index.html',
+  './admin.html',
+  './historial.html',
+  './franquicias.html',
+  './manifest.json',
+  './icons/icon-72.png',
+  './icons/icon-96.png',
+  './icons/icon-128.png',
+  './icons/icon-144.png',
+  './icons/icon-152.png',
+  './icons/icon-192.png',
+  './icons/icon-384.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-192.png',
+  './icons/icon-maskable-512.png'
 ];
 
-// Instalar el Service Worker y guardar en caché los archivos
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
-// Estrategia: Buscar primero en caché, si no está, ir a la red
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(response => {
+      if (response) return response;
+      return fetch(e.request).then(networkResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          if (e.request.url.startsWith('http')) {
+            cache.put(e.request, networkResponse.clone());
+          }
+          return networkResponse;
+        });
+      }).catch(() => caches.match('./index.html'));
+    })
   );
 });
-
